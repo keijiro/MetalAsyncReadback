18f6f4922b4a2ce7049c2b01148ad6064b56aa2c
Keijiro Takahashi <keijiro@gmail.com>
2018-05-07 11:06:02 +0900

    initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] ComputeShader _compute;
+
+    const int bufferSize = 1920 * 1080;
+
+    struct CopyBufferArgs
+    {
+        public IntPtr source;
+        public IntPtr destination;
+        public uint length;
+    };
+
+    ComputeBuffer _gpuBuffer;
+    CommandBuffer _command;
+    GCHandle _args;
+
+    IntPtr _copyBuffer;
+    int [] _managedBuffer;
+
+    void OnEnable()
+    {
+        _gpuBuffer = new ComputeBuffer(bufferSize, 4);
+        _command = new CommandBuffer();
+
+        _copyBuffer = BufferAccessor_Create(bufferSize * 4);
+
+        _managedBuffer = new int [bufferSize];
+    }
+
+    void OnDisable()
+    {
+        _gpuBuffer.Dispose();
+        _command.Dispose();
+
+        if (_args.IsAllocated) _args.Free();
+
+        BufferAccessor_Destroy(_copyBuffer);
+        _copyBuffer = IntPtr.Zero;
+
+        _managedBuffer = null;
+    }
+
+    void Update()
+    {
+        if (!_args.IsAllocated)
+        {
+            _args = GCHandle.Alloc(
+                new CopyBufferArgs {
+                    source = _gpuBuffer.GetNativeBufferPtr(),
+                    destination = _copyBuffer,
+                    length = bufferSize * 4
+                },
+                GCHandleType.Pinned
+            );
+        }
+
+        _compute.SetBuffer(0, "Destination", _gpuBuffer);
+        _compute.SetInt("FrameCount", Time.frameCount);
+        _compute.Dispatch(0, bufferSize / 64, 1, 1);
+
+        _command.Clear();
+        _command.IssuePluginEventAndData(
+            BufferAccessor_GetCopyBufferCallback(),
+            0, _args.AddrOfPinnedObject()
+        );
+
+        Graphics.ExecuteCommandBuffer(_command);
+
+        var pointer = BufferAccessor_GetContents(_copyBuffer);
+        Marshal.Copy(pointer, _managedBuffer, 0, bufferSize);
+        Debug.Log(_managedBuffer[bufferSize - 1]);
+    }
+
+    [DllImport("BufferAccessor")]
+    static extern IntPtr BufferAccessor_Create(uint size);
+
+    [DllImport("BufferAccessor")]
+    static extern void BufferAccessor_Destroy(IntPtr buffer);
+
+    [DllImport("BufferAccessor")]
+    static extern IntPtr BufferAccessor_GetContents(IntPtr buffer);
+
+    [DllImport("BufferAccessor")]
+    static extern IntPtr BufferAccessor_GetCopyBufferCallback();
+}
---
+++ Plugin/BufferAccessor.m
@@ -0,0 +1,78 @@
+#import <Metal/Metal.h>
+#import "Unity/IUnityGraphics.h"
+#import "Unity/IUnityGraphicsMetal.h"
+
+#pragma mark Device interface retrieval
+
+static IUnityInterfaces *s_interfaces;
+static IUnityGraphicsMetal *s_graphics;
+
+static id <MTLDevice> GetMetalDevice()
+{
+    if (!s_graphics) s_graphics = UNITY_GET_INTERFACE(s_interfaces, IUnityGraphicsMetal);
+    return s_graphics ? s_graphics->MetalDevice() : nil;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces *interfaces)
+{
+    s_interfaces = interfaces;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload(void)
+{
+    s_interfaces = NULL;
+    s_graphics = NULL;
+}
+
+#pragma mark Buffer management
+
+void *BufferAccessor_Create(uint32_t size)
+{
+    id <MTLBuffer> buffer = [GetMetalDevice() newBufferWithLength:size options:MTLResourceStorageModeManaged];
+    return (__bridge_retained void*)buffer;
+}
+
+void BufferAccessor_Destroy(void *ptr_buffer)
+{
+    id <MTLBuffer> buffer = (__bridge_transfer id <MTLBuffer>)ptr_buffer;
+    (void)buffer; // Just touching it; Will be automatically released.
+}
+
+void* BufferAccessor_GetContents(void *ptr_buffer)
+{
+    id <MTLBuffer> buffer = (__bridge id <MTLBuffer>)ptr_buffer;
+    return buffer.contents;
+}
+
+#pragma mark CopyBuffer event callback
+
+typedef struct
+{
+    void* source;
+    void* destination;
+    uint32_t length;
+}
+CopyBufferArgs;
+
+static void CopyBufferCallback(int evendID, void *data)
+{
+    if (GetMetalDevice() == nil) return;
+    if (data == NULL) return;
+
+    CopyBufferArgs *args = data;
+    if (args->source == NULL || args->destination == NULL) return;
+    
+    id <MTLBuffer> source = (__bridge id <MTLBuffer>)args->source;
+    id <MTLBuffer> destination = (__bridge id <MTLBuffer>)args->destination;
+    
+    s_graphics->EndCurrentCommandEncoder();
+    
+    id <MTLBlitCommandEncoder> blit = [s_graphics->CurrentCommandBuffer() blitCommandEncoder];
+    [blit copyFromBuffer:source sourceOffset:0 toBuffer:destination destinationOffset:0 size:args->length];
+    [blit endEncoding];
+}
+
+UnityRenderingEventAndData UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API BufferAccessor_GetCopyBufferCallback()
+{
+    return CopyBufferCallback;
+}
