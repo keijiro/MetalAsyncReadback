1dadad9040c87f7078c175c85b3a535724a0f41a
Keijiro Takahashi <keijiro@gmail.com>
2018-05-07 21:55:28 +0900

    Add test options.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,80 +5,96 @@ using UnityEngine.Rendering;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] ComputeShader _compute;
+    enum Mode { CopyBuffer, SharedBuffer, GetData }
 
-    const int bufferSize = 1920 * 1080;
+    [SerializeField] Mode _mode;
+    [SerializeField] int _bufferSize = 1920 * 1080;
+
+    [SerializeField, HideInInspector] ComputeShader _compute;
 
     struct CopyBufferArgs
     {
         public IntPtr source;
         public IntPtr destination;
-        public uint length;
+        public int length;
     };
 
     ComputeBuffer _gpuBuffer;
-    CommandBuffer _command;
-    GCHandle _args;
-
     IntPtr _copyBuffer;
     int [] _managedBuffer;
 
+    CommandBuffer _command;
+    GCHandle _pluginArgs;
+
     void OnEnable()
     {
-        _gpuBuffer = new ComputeBuffer(bufferSize, 4);
+        _gpuBuffer = new ComputeBuffer(_bufferSize, 4);
+        _copyBuffer = BufferAccessor_Create(_bufferSize * 4);
+        _managedBuffer = new int [_bufferSize];
         _command = new CommandBuffer();
-
-        _copyBuffer = BufferAccessor_Create(bufferSize * 4);
-
-        _managedBuffer = new int [bufferSize];
     }
 
     void OnDisable()
     {
         _gpuBuffer.Dispose();
-        _command.Dispose();
-
-        if (_args.IsAllocated) _args.Free();
+        _gpuBuffer = null;
 
         BufferAccessor_Destroy(_copyBuffer);
         _copyBuffer = IntPtr.Zero;
 
         _managedBuffer = null;
+
+        _command.Dispose();
+        _command = null;
+
+        if (_pluginArgs.IsAllocated) _pluginArgs.Free();
     }
 
     void Update()
     {
-        if (!_args.IsAllocated)
-        {
-            _args = GCHandle.Alloc(
-                new CopyBufferArgs {
-                    source = _gpuBuffer.GetNativeBufferPtr(),
-                    destination = _copyBuffer,
-                    length = bufferSize * 4
-                },
-                GCHandleType.Pinned
-            );
-        }
-
         _compute.SetBuffer(0, "Destination", _gpuBuffer);
         _compute.SetInt("FrameCount", Time.frameCount);
-        _compute.Dispatch(0, bufferSize / 64, 1, 1);
+        _compute.Dispatch(0, _bufferSize / 64, 1, 1);
 
-        _command.Clear();
-        _command.IssuePluginEventAndData(
-            BufferAccessor_GetCopyBufferCallback(),
-            0, _args.AddrOfPinnedObject()
-        );
+        if (_mode == Mode.CopyBuffer)
+        {
+            if (!_pluginArgs.IsAllocated)
+            {
+                _pluginArgs = GCHandle.Alloc(
+                    new CopyBufferArgs {
+                        source = _gpuBuffer.GetNativeBufferPtr(),
+                        destination = _copyBuffer,
+                        length = _bufferSize * 4
+                    },
+                    GCHandleType.Pinned
+                );
+            }
 
-        Graphics.ExecuteCommandBuffer(_command);
+            _command.Clear();
+            _command.IssuePluginEventAndData(
+                BufferAccessor_GetCopyBufferCallback(),
+                0, _pluginArgs.AddrOfPinnedObject()
+            );
+            Graphics.ExecuteCommandBuffer(_command);
 
-        var pointer = BufferAccessor_GetContents(_copyBuffer);
-        Marshal.Copy(pointer, _managedBuffer, 0, bufferSize);
-        Debug.Log(_managedBuffer[bufferSize - 1]);
+            var pointer = BufferAccessor_GetContents(_copyBuffer);
+            Marshal.Copy(pointer, _managedBuffer, 0, _bufferSize);
+        }
+
+        if (_mode == Mode.SharedBuffer)
+        {
+            var pointer = BufferAccessor_GetContents(_gpuBuffer.GetNativeBufferPtr());
+            Marshal.Copy(pointer, _managedBuffer, 0, _bufferSize);
+        }
+
+        if (_mode == Mode.GetData)
+            _gpuBuffer.GetData(_managedBuffer);
+
+        Debug.Log(_managedBuffer[_bufferSize - 1]);
     }
 
     [DllImport("BufferAccessor")]
-    static extern IntPtr BufferAccessor_Create(uint size);
+    static extern IntPtr BufferAccessor_Create(int size);
 
     [DllImport("BufferAccessor")]
     static extern void BufferAccessor_Destroy(IntPtr buffer);
