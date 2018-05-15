dcb64ea5ef9582eeda310835bd729c004c1fa7ad
Keijiro Takahashi <keijiro@gmail.com>
2018-05-15 17:35:07 +0900

    Update iOS plugin code.

--- Assets/Plugin/iOS/BufferAccessor.m
+++ Assets/Plugin/iOS/BufferAccessor.m
@@ -3,8 +3,6 @@
 #import "IUnityGraphicsMetal.h"
 #import "UnityAppController.h"
 
-//#define USE_MANAGED_STORAGE
-
 #pragma mark Device interface retrieval
 
 static IUnityInterfaces *s_interfaces;
@@ -27,7 +25,9 @@ void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload(void)
     s_graphics = NULL;
 }
 
-#pragma mark App controller subclass
+#if PLATFORM_IOS
+
+#pragma mark App controller subclasssing
 
 @interface MyAppController : UnityAppController
 {
@@ -44,61 +44,45 @@ void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload(void)
 
 IMPL_APP_CONTROLLER_SUBCLASS(MyAppController);
 
-#pragma mark Buffer management
-
-void *BufferAccessor_Create(int32_t size)
-{
-#ifdef USE_MANAGED_STORAGE
-    id <MTLBuffer> buffer = [GetMetalDevice() newBufferWithLength:size options:MTLResourceStorageModeManaged];
-#else
-    id <MTLBuffer> buffer = [GetMetalDevice() newBufferWithLength:size options:MTLResourceStorageModeShared];
 #endif
-    return (__bridge_retained void*)buffer;
-}
-
-void BufferAccessor_Destroy(void *ptr_buffer)
-{
-    id <MTLBuffer> buffer = (__bridge_transfer id <MTLBuffer>)ptr_buffer;
-    (void)buffer; // Just touching it; Will be automatically released.
-}
-
-void* BufferAccessor_GetContents(void *ptr_buffer)
-{
-    id <MTLBuffer> buffer = (__bridge id <MTLBuffer>)ptr_buffer;
-    return buffer.contents;
-}
 
 #pragma mark CopyBuffer event callback
 
 typedef struct
 {
-    void* source;
-    void* destination;
+    void* source;       // id <MTLBuffer>
+    void* destination;  // byte*
     int32_t length;
 }
 CopyBufferArgs;
 
 static void CopyBufferCallback(int evendID, void *data)
 {
-    if (GetMetalDevice() == nil) return;
-    if (data == NULL) return;
-
-    CopyBufferArgs *args = data;
-    if (args->source == NULL || args->destination == NULL) return;
+    if (GetMetalDevice() == nil || data == NULL) return;
     
-    id <MTLBuffer> source = (__bridge id <MTLBuffer>)args->source;
-    id <MTLBuffer> destination = (__bridge id <MTLBuffer>)args->destination;
+    CopyBufferArgs args = *(CopyBufferArgs*)data;
+    if (args.source == NULL || args.destination == NULL) return;
     
+    id <MTLBuffer> source = (__bridge id <MTLBuffer>)args.source;
+    
+#if PLATFORM_OSX
+    // Synchronize the managed buffer in case of macOS.
     s_graphics->EndCurrentCommandEncoder();
-    
     id <MTLBlitCommandEncoder> blit = [s_graphics->CurrentCommandBuffer() blitCommandEncoder];
-    // NOTE: Compute buffers managed by Unity have a single int counter in the head of the buffer.
-    // To skip this part, a 4-byte offset is added to the sourceOffset argument.
-    [blit copyFromBuffer:source sourceOffset:4 toBuffer:destination destinationOffset:0 size:args->length];
-#ifdef USE_MANAGED_STORAGE
-    [blit synchronizeResource:destination];
-#endif
+    [blit synchronizeResource:source];
     [blit endEncoding];
+#endif
+    
+    // Add command completion handler that kicks in the async copy block.
+    [s_graphics->CurrentCommandBuffer() addCompletedHandler:^(id<MTLCommandBuffer> _Nonnull cb) {
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), ^{
+            // NOTE: Compute buffers managed by Unity have a single int counter at the head of the buffer.
+            // To skip this part, a 4-byte offset is added to the sourceOffset argument.
+            // Also we use the first 4 bytes of the destination buffer to store the result.
+            memcpy(args.destination + 4, source.contents + 4, args.length);
+            *(uint32_t *)args.destination = args.length;
+        });
+    }];
 }
 
 UnityRenderingEventAndData UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API BufferAccessor_GetCopyBufferCallback()
